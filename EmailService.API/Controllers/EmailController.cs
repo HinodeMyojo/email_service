62702756e74c19f93c1818efb1bf2b3cada180f5
Egor Semenov <135758742+HinodeMyojo@users.noreply.github.com>
2024-07-23 18:03:15 +0300

    add email dispatcher

---
+++ EmailService.API/Controllers/EmailController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmailService.API.Controllers
+{
+    public class EmailController : ControllerBase
+    {
+        private ILogger<EmailController> _logger;
+        public EmailController(ILogger<EmailController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("send")]
+        public async Task<IActionResult> SendEmail()
+        {
+
+        }
+    }
+}
---
+++ EmailService_Core/Abstractions/IEmailDispatcher.cs
@@ -0,0 +1,9 @@
+﻿using EmailService_Core.Models;
+
+namespace EmailService_Core.Abstractions
+{
+    public interface IEmailDispatcher
+    {
+        void SendEmail(Message email);
+    }
+}
